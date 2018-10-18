f308555fd21954e34ac4291ace335d647a879b93
j- <j@skeoh.com>
2018-10-18 15:49:11 +1100

    Add router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../routes/Home';
 
 const App: React.StatelessComponent = () => (
-	<div className="App">
-		{null}
-	</div>
+	<Router>
+		<div className="App">
+			<h1>Inputs</h1>
+
+			<Route exact={true} path="/" component={Home} />
+		</div>
+	</Router>
 );
 
 export default App;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { BrowserRouterProps } from 'react-router-dom';
+
+const Home: React.StatelessComponent<BrowserRouterProps> = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+);
+
+export default Home;
