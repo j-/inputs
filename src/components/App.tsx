fddcc5510def35e18868c507a9b5af0371115d79
j- <j@skeoh.com>
2018-10-18 16:02:08 +1100

    Add navigation component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from './Navigation';
 import Home from '../routes/Home';
 
 const App: React.StatelessComponent = () => (
 	<Router>
 		<div className="App">
 			<h1>Inputs</h1>
+
+			<Navigation />
 
 			<Route exact={true} path="/" component={Home} />
 		</div>
