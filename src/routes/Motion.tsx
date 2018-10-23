1675c2bc6ccb454c733549eea383a21275544649
j- <j@skeoh.com>
2018-10-23 14:43:36 +1100

    Add more properties to motion output

--- src/routes/Motion.tsx
+++ src/routes/Motion.tsx
@@ -20,6 +20,7 @@ export default class Motion extends React.Component<BrowserRouterProps, State> {
 	}
 
 	render () {
+		const { interval, acceleration, accelerationIncludingGravity, rotationRate } = this.state.event;
 		return (
 			<div>
 				<h2>Motion</h2>
@@ -27,14 +28,47 @@ export default class Motion extends React.Component<BrowserRouterProps, State> {
 				<h3>Values</h3>
 
 				<dl>
+					<dt><code>interval</code></dt>
+					<dd><code>{interval || <NA />}</code></dd>
+				</dl>
+
+				<h4>Acceleration</h4>
+
+				<dl>
 					<dt><code>x</code></dt>
-					<dd><code>{this.state.event.acceleration && this.state.event.acceleration.x || <NA />}</code></dd>
+					<dd><code>{acceleration && acceleration.x || <NA />}</code></dd>
 
 					<dt><code>y</code></dt>
-					<dd><code>{this.state.event.acceleration && this.state.event.acceleration.y || <NA />}</code></dd>
+					<dd><code>{acceleration && acceleration.y || <NA />}</code></dd>
 
 					<dt><code>z</code></dt>
-					<dd><code>{this.state.event.acceleration && this.state.event.acceleration.z || <NA />}</code></dd>
+					<dd><code>{acceleration && acceleration.z || <NA />}</code></dd>
+				</dl>
+
+				<h4>Acceleration (including gravity)</h4>
+
+				<dl>
+					<dt><code>x</code></dt>
+					<dd><code>{accelerationIncludingGravity && accelerationIncludingGravity.x || <NA />}</code></dd>
+
+					<dt><code>y</code></dt>
+					<dd><code>{accelerationIncludingGravity && accelerationIncludingGravity.y || <NA />}</code></dd>
+
+					<dt><code>z</code></dt>
+					<dd><code>{accelerationIncludingGravity && accelerationIncludingGravity.z || <NA />}</code></dd>
+				</dl>
+
+				<h4>Rotation rate</h4>
+
+				<dl>
+					<dt><code>alpha</code></dt>
+					<dd><code>{rotationRate && rotationRate.alpha || <NA />}</code></dd>
+
+					<dt><code>beta</code></dt>
+					<dd><code>{rotationRate && rotationRate.beta || <NA />}</code></dd>
+
+					<dt><code>gamma</code></dt>
+					<dd><code>{rotationRate && rotationRate.gamma || <NA />}</code></dd>
 				</dl>
 			</div>
 		);
